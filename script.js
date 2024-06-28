8617accd9954d938077a7bad4dbb449055a9bba2
Matt Marzano <122555133+mattmarz94@users.noreply.github.com>
2024-06-28 16:17:11 -0400

    created elemtns in js for a popup which occurs when adding/removing a movie. Then styld the popup in the css file.

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // API Key e099eae9
 
 const movieForm = document.querySelector("#movie-search-form")
-const moviesContainer = document.getElementById('movies-container')
+const moviesContainer = document.querySelector('#movies-container')
+const watchlistContainer = document.querySelector(".watchlist-container")
 
 let moviesArray = []
 let movieWatchlist = []
@@ -11,31 +12,32 @@ function handleMovieInfo(movie){
     const moviesIds = movie.map(m => m.imdbID)
 
     moviesIds.forEach(id =>{
-        if(!moviesArray.some(function (film){
-            return film.Title === film.Title
-        }))
-        fetch(`http://www.omdbapi.com/?apikey=e099eae9&i=${id}`)
-            .then(res => res.json())
-            .then(data => {
-                
-                moviesArray.push(data)
-                
-            })
+        if(!moviesArray.some( film => film.Title === film.Title )){
+            fetch(`http://www.omdbapi.com/?apikey=e099eae9&i=${id}`)
+                .then(res => res.json())
+                .then(data => {
+                    
+                    moviesArray.push(data)
+                    
+                })
+            }
         })
         renderMovies()
 }
 
-movieForm.addEventListener("submit", (e) => {
-    e.preventDefault()
-    const userInput = document.getElementById("user-input")
+if(movieForm){
+    movieForm.addEventListener("submit", (e) => {
+        e.preventDefault()
+        const userInput = document.getElementById("user-input")
 
-    fetch(`http://www.omdbapi.com/?apikey=e099eae9&s=${userInput.value}`)
-        .then( res => res.json() )
-        .then( data => {
-                    
-            handleMovieInfo(data.Search)
-        })
-})
+        fetch(`http://www.omdbapi.com/?apikey=e099eae9&s=${userInput.value}`)
+            .then( res => res.json() )
+            .then( data => {
+                        
+                handleMovieInfo(data.Search)
+            })
+    })
+}
 
 function renderMovies(){
     let renderedMoviesHtml = ''
@@ -67,7 +69,7 @@ function renderMovies(){
     moviesContainer.innerHTML = renderedMoviesHtml
 }
 
-document.addEventListener('click', (e) => {
+moviesContainer.addEventListener('click', (e) => {
     addToWatchlist(e.target.dataset.watchlist)
 })
 
@@ -76,7 +78,15 @@ function addToWatchlist(id){
         if (movie.imdbID === id){
             if(!movieWatchlist.some(film => film.Title === movie.Title))
                 movieWatchlist.push(movie)
-                alert("added to watchlist")
+                const popup = document.createElement("div")
+                popup.classList.add("pop-up")
+                popup.textContent = "Movie added to watchlist!"
+
+                document.body.appendChild(popup);
+
+                setTimeout(() => {
+                    popup.remove()
+                }, 2000);
                 updateWatchlist()
         }
     }
@@ -84,4 +94,68 @@ function addToWatchlist(id){
 
 function updateWatchlist(){
     localStorage.setItem("watchlist", JSON.stringify(movieWatchlist))
+}
+
+function renderWatchlist(){
+    let renderedMoviesHtml = ''
+
+    for (let movie of movieWatchlist){
+        renderedMoviesHtml += `
+        <div class="rendered-movies-container">
+            <img src = "${movie.Poster}">
+            <div class="movie-container">
+                <div class="title-container">
+                    <h2>${movie.Title}</h2>
+                    <i class="fa-solid fa-star"></i>
+                    <p class="movie-rating">${movie.imdbRating}</p>
+                </div>
+                <div class = "time-genre-add-container">
+                    <p class ="movie-runtime">${movie.Runtime}</p>
+                    <p class ="movie-genre">${movie.Genre}</p>
+                    <i class="fa-solid fa-square-minus" data-remove=${movie.imdbID}></i>
+                    <p class="watchlist" data-remove=${movie.imdbID}>Remove</p>
+                </div>
+                <div class ="movie-plot-container">
+                    <p class="movie-plot">${movie.Plot}</p>
+                </div>
+            </div>
+        </div>
+        <hr/>
+        `
+    }
+    if(watchlistContainer){
+    watchlistContainer.innerHTML = renderedMoviesHtml
+    }
+}
+
+function removeMovie(id){
+    const indexToRemove = movieWatchlist.findIndex(movie => movie.imdbID === id)
+    if (indexToRemove !== -1){
+        movieWatchlist.splice(indexToRemove, 1)
+        updateWatchlist()
+        renderWatchlist()
+    }
+}
+
+if(watchlistContainer){
+    watchlistContainer.addEventListener("click", (e) => {
+        if(e.target.dataset.remove){
+            removeMovie(e.target.dataset.remove)
+            const popup = document.createElement("div")
+            popup.classList.add("pop-up")
+            popup.textContent = "Movie sucessfully removed"
+
+            document.body.appendChild(popup);
+
+            setTimeout(() => {
+                popup.remove()
+            }, 2000);
+        }
+    })
+}
+
+const watchlistMovies = localStorage.getItem("watchlist")
+if (watchlistMovies){
+    movieWatchlist = JSON.parse(watchlistMovies)
+    renderWatchlist()
 }
