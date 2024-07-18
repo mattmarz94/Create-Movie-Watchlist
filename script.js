0a2523158bfbed24443d2ca8da38ed781c03a660
Matt Marzano <122555133+mattmarz94@users.noreply.github.com>
2024-07-18 14:19:51 -0400

    reformatted the files for better readability.

--- script.js
+++ script.js
@@ -1,53 +1,57 @@
 // API Key e099eae9
 
-const movieForm = document.querySelector("#movie-search-form")
-const moviesContainer = document.querySelector('#movies-container')
-const watchlistContainer = document.querySelector(".watchlist-container")
+const movieForm = document.querySelector("#movie-search-form");
+const moviesContainer = document.querySelector("#movies-container");
+const watchlistContainer = document.querySelector(".watchlist-container");
 
-let moviesArray = []
-let movieWatchlist = []
+let moviesArray = [];
+let movieWatchlist = [];
 
 async function handleMovieInfo(movie) {
-    const moviesIds = movie.map(m => m.imdbID);
+  const moviesIds = movie.map((m) => m.imdbID);
 
-    await Promise.all(moviesIds.map(async id => {
-        if (!moviesArray.some(film => film.Title === film.Title)) {
-            try {
-                const response = await fetch(`http://www.omdbapi.com/?apikey=e099eae9&i=${id}`);
-                const data = await response.json();
-                moviesArray.push(data);
-            } catch (error) {
-                console.error('Error fetching movie data:', error);
-            }
+  await Promise.all(
+    moviesIds.map(async (id) => {
+      if (!moviesArray.some((film) => film.Title === film.Title)) {
+        try {
+          const response = await fetch(
+            `http://www.omdbapi.com/?apikey=e099eae9&i=${id}`
+          );
+          const data = await response.json();
+          moviesArray.push(data);
+        } catch (error) {
+          console.error("Error fetching movie data:", error);
         }
-    }));
+      }
+    })
+  );
 
-    renderMovies();
+  renderMovies();
 }
-
 
 if (movieForm) {
-    movieForm.addEventListener("submit", async (e) => {
-        e.preventDefault();
-        const userInput = document.getElementById("user-input");
+  movieForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const userInput = document.getElementById("user-input");
 
-        try {
-            const response = await fetch(`http://www.omdbapi.com/?apikey=e099eae9&s=${userInput.value}`);
-            const data = await response.json();
-            
-            handleMovieInfo(data.Search);
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
-    });
+    try {
+      const response = await fetch(
+        `http://www.omdbapi.com/?apikey=e099eae9&s=${userInput.value}`
+      );
+      const data = await response.json();
+
+      handleMovieInfo(data.Search);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  });
 }
 
+function renderMovies() {
+  let renderedMoviesHtml = "";
 
-function renderMovies(){
-    let renderedMoviesHtml = ''
-
-    for(let movie of moviesArray) {
-        renderedMoviesHtml += `
+  for (let movie of moviesArray) {
+    renderedMoviesHtml += `
         <div class="rendered-movies-container">
             <img src = "${movie.Poster}">
             <div class="movie-container">
@@ -68,55 +72,54 @@ function renderMovies(){
             </div>
         </div>
         <hr/>
-        `
+        `;
+  }
+  moviesContainer.innerHTML = renderedMoviesHtml;
+}
+
+moviesContainer.addEventListener("click", (e) => {
+  addToWatchlist(e.target.dataset.watchlist);
+});
+
+function addToWatchlist(id) {
+  for (let movie of moviesArray) {
+    if (movie.imdbID === id) {
+      if (!movieWatchlist.some((film) => film.Title === movie.Title)) {
+        movieWatchlist.push(movie);
+        const popup = document.createElement("div");
+        popup.classList.add("pop-up");
+        popup.textContent = "Movie added to watchlist!";
+
+        document.body.appendChild(popup);
+
+        setTimeout(() => {
+          popup.remove();
+        }, 2000);
+        updateWatchlist();
+      } else {
+        const popup = document.createElement("div");
+        popup.classList.add("pop-up");
+        popup.textContent = "Movie already added to watchlist!";
+        document.body.appendChild(popup);
+
+        setTimeout(() => {
+          popup.remove();
+        }, 2000);
+        updateWatchlist();
+      }
     }
-    moviesContainer.innerHTML = renderedMoviesHtml
+  }
 }
 
-moviesContainer.addEventListener('click', (e) => {
-    addToWatchlist(e.target.dataset.watchlist)
-})
-
-function addToWatchlist(id){
-    for (let movie of moviesArray){
-        if (movie.imdbID === id){
-            if(!movieWatchlist.some(film => film.Title === movie.Title)){
-                movieWatchlist.push(movie)
-                const popup = document.createElement("div")
-                popup.classList.add("pop-up")
-                popup.textContent = "Movie added to watchlist!"
-
-                document.body.appendChild(popup);
-
-                setTimeout(() => {
-                    popup.remove()
-                }, 2000);
-                updateWatchlist()
-            }else{
-                const popup = document.createElement("div")
-                popup.classList.add("pop-up")
-                popup.textContent = "Movie already added to watchlist!"
-                document.body.appendChild(popup);
-
-                setTimeout(() => {
-                    popup.remove()
-                }, 2000);
-                updateWatchlist()
-            }
-        }
-        
-    }
+function updateWatchlist() {
+  localStorage.setItem("watchlist", JSON.stringify(movieWatchlist));
 }
 
-function updateWatchlist(){
-    localStorage.setItem("watchlist", JSON.stringify(movieWatchlist))
-}
+function renderWatchlist() {
+  let renderedMoviesHtml = "";
 
-function renderWatchlist(){
-    let renderedMoviesHtml = ''
-
-    for (let movie of movieWatchlist){
-        renderedMoviesHtml += `
+  for (let movie of movieWatchlist) {
+    renderedMoviesHtml += `
         <div class="rendered-movies-container">
             <img src = "${movie.Poster}">
             <div class="movie-container">
@@ -137,41 +140,43 @@ function renderWatchlist(){
             </div>
         </div>
         <hr/>
-        `
-    }
-    if(watchlistContainer){
-    watchlistContainer.innerHTML = renderedMoviesHtml
-    }
+        `;
+  }
+  if (watchlistContainer) {
+    watchlistContainer.innerHTML = renderedMoviesHtml;
+  }
 }
 
-function removeMovie(id){
-    const indexToRemove = movieWatchlist.findIndex(movie => movie.imdbID === id)
-    if (indexToRemove !== -1){
-        movieWatchlist.splice(indexToRemove, 1)
-        updateWatchlist()
-        renderWatchlist()
+function removeMovie(id) {
+  const indexToRemove = movieWatchlist.findIndex(
+    (movie) => movie.imdbID === id
+  );
+  if (indexToRemove !== -1) {
+    movieWatchlist.splice(indexToRemove, 1);
+    updateWatchlist();
+    renderWatchlist();
+  }
+}
+
+if (watchlistContainer) {
+  watchlistContainer.addEventListener("click", (e) => {
+    if (e.target.dataset.remove) {
+      removeMovie(e.target.dataset.remove);
+      const popup = document.createElement("div");
+      popup.classList.add("pop-up");
+      popup.textContent = "Movie sucessfully removed";
+
+      document.body.appendChild(popup);
+
+      setTimeout(() => {
+        popup.remove();
+      }, 2000);
     }
+  });
 }
 
-if(watchlistContainer){
-    watchlistContainer.addEventListener("click", (e) => {
-        if(e.target.dataset.remove){
-            removeMovie(e.target.dataset.remove)
-            const popup = document.createElement("div")
-            popup.classList.add("pop-up")
-            popup.textContent = "Movie sucessfully removed"
-
-            document.body.appendChild(popup);
-
-            setTimeout(() => {
-                popup.remove()
-            }, 2000);
-        }
-    })
-}
-
-const watchlistMovies = localStorage.getItem("watchlist")
-if (watchlistMovies){
-    movieWatchlist = JSON.parse(watchlistMovies)
-    renderWatchlist()
+const watchlistMovies = localStorage.getItem("watchlist");
+if (watchlistMovies) {
+  movieWatchlist = JSON.parse(watchlistMovies);
+  renderWatchlist();
 }
